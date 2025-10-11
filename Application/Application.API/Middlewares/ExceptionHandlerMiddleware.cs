5077082c3462b1742243225927837865ca3b03cd
souheyl gouadria <souheyl.gouadria@supcom.tn>
2025-10-11 13:02:07 +0100

    add ExceptionHandler middleware

---
+++ Application/Application.API/Middlewares/ExceptionHandlerMiddleware.cs
@@ -0,0 +1,37 @@
+﻿using System.Net;
+
+namespace Application.API.Middlewares
+{
+    public class ExceptionHandlerMiddleware
+    {
+        private readonly ILogger<ExceptionHandlerMiddleware> _logger;
+        private readonly RequestDelegate _next;
+        public ExceptionHandlerMiddleware(ILogger<ExceptionHandlerMiddleware> logger, RequestDelegate next)
+        {
+            _logger = logger;
+            _next = next;
+        }
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch (Exception ex)
+            {
+                var errorId = Guid.NewGuid();
+                _logger.LogError(ex, $"{errorId} : {ex.Message}");
+                // return custom error response
+                httpContext.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                httpContext.Response.ContentType = "application/json";
+
+                var error = new
+                {
+                    Id = errorId,
+                    ErrorMessage = "Something went wrong !"
+                };
+                await httpContext.Response.WriteAsJsonAsync(error);
+            }
+        }
+    }
+}
